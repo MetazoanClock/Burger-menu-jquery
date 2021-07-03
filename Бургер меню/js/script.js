f659f050c828ea503d203625998eba9fe6cdbbdf
MetazoanClock <72189863+MetazoanClock@users.noreply.github.com>
2021-07-03 19:22:17 +0300

    Update script.js

--- Бургер меню/js/script.js
+++ Бургер меню/js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     $('.menu-burger__header').click(function() {
     $('.menu-burger__header,.header_menu').toggleClass('active');
     $('body').toggleClass('lock');
-    $('body').removeClass('lock');
     });
   });
   
   $('.header_menu').click(function(){
     $('.menu-burger__header,.header_menu').removeClass('active');
+     $('body').toggleClass('lock');
     $('body').removeClass('lock');
   });
   
